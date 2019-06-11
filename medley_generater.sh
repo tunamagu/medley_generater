87041f68813ffcc572fbfff55baec0f313b88cb9
ツナマグ <37657831+tunamagu@users.noreply.github.com>
2019-06-11 23:06:13 +0900

    コメントアウト修正

--- medley_generater.sh
+++ medley_generater.sh
@@ -10,8 +10,7 @@ do
 	imput_number=`echo ${file} | cut -c 1-2`
 	start_number=$(expr $imput_number - 1)
 	count=$((++count))
-#	end_number=$(expr $start_number + 10)
-	# ffmpeg -y -i "$file" -codec:a copy -ss $start_number -t 11 "./after_cut/$count.mp3"
+	ffmpeg -y -i "$file" -codec:a copy -ss $start_number -t 11 "./after_cut/$count.mp3"
 done
 
 cd after_cut
@@ -23,11 +22,10 @@ for file in *.mp3
 do
 	file_new=`echo 'fade'${file}'.mp3' | sed -e 's/.mp3$//g'`
 	# フェード処理
-	# ffmpeg -i "$file" -af afade=t=in:st=0:d=1,afade=t=out:st=10:d=1,dynaudnorm "$file_new"
-	# rm ${file}
+	ffmpeg -i "$file" -af afade=t=in:st=0:d=1,afade=t=out:st=10:d=1,dynaudnorm "$file_new"
+	rm ${file}
 
-	# cut_file_list="${cut_file_list}-i ${file_new} "
-	cut_file_list="${cut_file_list}-i ${file} "
+	cut_file_list="${cut_file_list}-i ${file_new} "
 	count=$((++count))
 
 	# テスト用件数指定

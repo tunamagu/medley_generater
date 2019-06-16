67d3e713ea8504480b6744e551d632254bb37785
ツナマグ <37657831+tunamagu@users.noreply.github.com>
2019-06-16 17:34:59 +0900

    音質が悪くなる問題を修正

--- opening_medley_generater.sh
+++ opening_medley_generater.sh
@@ -19,7 +19,7 @@ do
 	if [ -e ./after_cut/$count.mp4 ]; then
 		continue
 	else
-		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,dynaudnorm -c:a aac -b:a 160k "./after_cut/$count.mp4"
+		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,volume=3dB -c:a mp3 -b:a 320k "./after_cut/$count.mp4"
 	fi
 
 done
@@ -27,11 +27,5 @@ done
 cd after_cut
 
 # 結合処理
-command="ffmpeg ${cut_file_list} -filter_complex concat=n=${count}:v=1:a=1 -c:a aac -b:a 160k output.mp4"
+command="ffmpeg ${cut_file_list} -filter_complex concat=n=${count}:v=1:a=1 -c:a mp3 -b:a 320k output.mp4"
 ${command}
-
-#array=`find . -type f -name *.mp3`
-#array=`ls *.mp3`
-#num_array=${#array[*]}
-#random_n=%(RANDOM%num_array)
-#shuffle

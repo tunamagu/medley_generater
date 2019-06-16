749b3387cb577c8f1fc2b977fcc9e4987e4a479c
ツナマグ <37657831+tunamagu@users.noreply.github.com>
2019-06-17 03:14:17 +0900

    ランダム処理実装

--- opening_medley_generater.sh
+++ opening_medley_generater.sh
@@ -14,18 +14,26 @@ do
 	start_number=$(expr $imput_number - 1)
 
 	count=$((++count))
-	cut_file_list="${cut_file_list}-i ${count}.mp4 "
 
 	if [ -e ./after_cut/$count.mp4 ]; then
 		continue
 	else
-		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,volume=3dB -c:a mp3 -b:a 320k "./after_cut/$count.mp4"
+		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,volume=5dB -c:a mp3 -b:a 320k "./after_cut/$count.mp4"
 	fi
 
 done
+
+for n in `shuf -i 1-$count`
+do
+	cut_file_list="${cut_file_list}-i ${n}.mp4 "
+done
+
 
 cd after_cut
 
 # 結合処理
 command="ffmpeg ${cut_file_list} -filter_complex concat=n=${count}:v=1:a=1 -c:a mp3 -b:a 320k output.mp4"
+# command="ffmpeg -f concat ${cut_file_list} -c:v copy -af volume=5dB -c:a mp3 -b:a 320k -map 0:v -map 0:a output.mp4"
+${command}
+command="ffmpeg after_cut/output.mp4 -af dynaudnorm -c:v copy -c:a mp3 -b:a 320k after_cut/output_v.mp4"
 ${command}

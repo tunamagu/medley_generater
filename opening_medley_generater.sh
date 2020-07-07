90cd3418ccfcfcaf1ca22f63b894ea075d54cbd0
ツナマグ <37657831+tunamagu@users.noreply.github.com>
2020-07-07 15:52:30 +0900

    Add files via upload

--- opening_medley_generater.sh
+++ opening_medley_generater.sh
@@ -18,7 +18,7 @@ do
 	if [ -e ./after_cut/$count.mp4 ]; then
 		continue
 	else
-		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,volume=5dB -c:a mp3 -b:a 320k "./after_cut/$count.mp4"
+		ffmpeg -y -ss $start_number -i "$file" -t $cut_time -s 1280x720 -aspect 16:9 -vf fade=in:st=0:d=1,fade=out:st="$fadeout_start":d=1 -af afade=t=in:st=0:d=1,afade=t=out:st="$fadeout_start":d=1,volume=5dB -c:a mp3 -b:a 320k "./after_cut/$count.mp4"
 	fi
 
 done
@@ -35,5 +35,5 @@ cd after_cut
 command="ffmpeg ${cut_file_list} -filter_complex concat=n=${count}:v=1:a=1 -c:a mp3 -b:a 320k output.mp4"
 # command="ffmpeg -f concat ${cut_file_list} -c:v copy -af volume=5dB -c:a mp3 -b:a 320k -map 0:v -map 0:a output.mp4"
 ${command}
-command="ffmpeg after_cut/output.mp4 -af dynaudnorm -c:v copy -c:a mp3 -b:a 320k after_cut/output_v.mp4"
+command="ffmpeg -i output.mp4 -af dynaudnorm -c:v copy -c:a mp3 -b:a 320k output_v.mp4"
 ${command}
